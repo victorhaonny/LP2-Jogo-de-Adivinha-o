853fad04fef8f2db84e4b5f96f7e2ba0ef6eeaed
victorhaonny <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>
2025-09-18 16:47:32 -0300

    Ideias de Menu de opçoes de dificuldade

--- Jogo_Adivinha_V1/main.c
+++ Jogo_Adivinha_V1/main.c
@@ -1,2 +1,28 @@
+//############
+//Data:18/09/2025
+//Aluno:Victor 
+//###########
 #include <stdio.h>
 #include <stdlib.h>
+//O main vai se a base, ele vai utilizar ou um if else ou um switch case para controla o menu
+    int main(void){
+
+        int Escolhar;
+
+        printf("Escolhar Nivel: ");
+        scanf("%d",&Escolhar);
+
+        switch(Escolhar){
+            case 1:
+            break;
+            case 2:
+            break;
+            case 3:
+            break;
+            default:
+            printf("Esss Nivel não existe");
+            return main();
+        }
+
+        return 0;
+    }
