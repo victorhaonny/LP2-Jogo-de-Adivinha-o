2f3ea3bb12569a8a1305b265da223890dfd4d19f
Jr-Reginaldo999 <jrreginaldo999@gmail.com>
2025-10-06 15:43:35 -0300

    Teve mudança nas funcoes.c

--- Jogo_Adivinha_V1/funcoes.c
+++ Jogo_Adivinha_V1/funcoes.c
@@ -25,43 +25,43 @@ void dificuldade(char a, int *b)
 void Selecionando_Categorias(char a, Perguntas *b)
 {
     if (a == 'A' || a == 'F' || a == 'S')
-        *b = Perguntas_Categorias(a);
+        *b = *Perguntas_Categorias(a);
     else
     {
         printf("O Padrão vai ser filmes");
-        *b = Perguntas_Categorias('F');
+        *b = *Perguntas_Categorias('F');
     }
 }
 
 // Função para passa as perguntas Certas na Struct do jeito que o usuario pediu.
 
-Perguntas Perguntas_Categorias(char a)
+Perguntas *Perguntas_Categorias(char a)
 {
-    if (a == 'F')
-    {
-        Perguntas Filmes[3] = {
-            {"Frase 1", {"A", "A", "A"}, 'A'},
-            {"Frase 2", {"B", "B", "B"}, 'B'},
-            {"Frase 3", {"C", "C", "C"}, 'C'}};
-        return Filmes[3];
+    static Perguntas Filmes[3] = {
+        {"Frase 1", {"A", "A", "A"}, 'A'},
+        {"Frase 2", {"B", "B", "B"}, 'B'},
+        {"Frase 3", {"C", "C", "C"}, 'C'}
+    };
+    static Perguntas Series[3] = {
+        {"Frase 1", {"A", "B", "C"}, 'A'},
+        {"Frase 2", {"A", "B", "C"}, 'B'},
+        {"Frase 3", {"A", "B", "C"}, 'C'}
+    };
+    static Perguntas Aleatorio[3] = {
+        {"Frase 1", {"A", "B", "C"}, 'A'},
+        {"Frase 2", {"A", "B", "C"}, 'B'},
+        {"Frase 3", {"A", "B", "C"}, 'C'}
+    };
+
+        if (a == 'F')
+            return Filmes;
+        if (a == 'S')
+            return Series;
+        else if (a == 'A')
+            return Aleatorio;
+
+    return Filmes;
     }
-    else if (a == 'S')
-    {
-        Perguntas Series[3] = {
-            {"Frase 1", {"A", "B", "C"}, 'A'},
-            {"Frase 2", {"A", "B", "C"}, 'B'},
-            {"Frase 3", {"A", "B", "C"}, 'C'}};
-        return Series[3];
-    }
-    else if (a == 'A')
-    {
-        Perguntas Aleatorio[3] = {
-            {"Frase 1", {"A", "B", "C"}, 'A'},
-            {"Frase 2", {"A", "B", "C"}, 'B'},
-            {"Frase 3", {"A", "B", "C"}, 'C'}};
-        return Aleatorio[3];
-    }
-}
 
 // Função para mostra na tela as perguntas
 
@@ -71,14 +71,14 @@ void Mostrando_Perguntas_Tela(int a, int b, Perguntas d[])
 
     for (int i = 0; i < a; i++)
     {
-        printf("Pergunta: %s", d[i].questoes);
+        printf("Pergunta: %s\n", d[i].questoes);
 
         for (int j = 0; j < a; j++)
         {
-            printf("\n%s", d[j].opc);
+            printf("\n%s", d[i].opc[j]);
         }
         printf("\nResposta: ");
-        scanf("%c", d[i].respostas);
+        scanf("%c", &Resposta);
 
         if (Resposta != d[i].respostas)
         {
@@ -98,6 +98,6 @@ void Ganhou_Perdeu(int vida, int acertos)
 
     if (vida == 0)
         printf("GAME OVER");
-    else if (acertos = vida)
+    else if (acertos == vida)
         printf("YOU WIN");
 }
