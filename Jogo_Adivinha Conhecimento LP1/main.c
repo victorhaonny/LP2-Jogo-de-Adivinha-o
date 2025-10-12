32bb72ee0d835497a90884c0c218d7b629a553f4
Victor Haonny <57414856+victorhaonny@users.noreply.github.com>
2025-10-12 18:56:10 -0300

    Cabeçalho

--- Jogo_Adivinha Conhecimento LP1/main.c
+++ Jogo_Adivinha Conhecimento LP1/main.c
@@ -1,3 +1,9 @@
+// ########################################
+//  CIC UESC - LP2
+//  Aluno: Victor e Reginaldo
+//  Data: 09/10/2025
+// ########################################
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
